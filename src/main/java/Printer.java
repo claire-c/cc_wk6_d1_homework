cbe4e2e7df8264bc93deb6618579a40d447cc3d4
Claire <cconnachan@gmail.com>
2018-04-09 20:16:07 +0100

    printer passed first test

---
+++ src/main/java/Printer.java
@@ -0,0 +1,16 @@
+public class Printer {
+
+    private int paper;
+
+    public Printer(int paper){
+        this.paper = paper;
+
+    }
+
+    public int getPaper(){
+        return this.paper;
+    }
+
+
+}
+
---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrinterTest {
+
+    private Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(50);
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(50,printer.getPaper());
+    }
+
+}
