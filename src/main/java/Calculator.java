b28dba5a3f74ce0f5607ed8cae4fd5215da2dbb0
Claire <cconnachan@gmail.com>
2018-04-09 16:50:28 +0100

    calculator passed first test

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+
+    public Calculator(){
+
+    }
+
+    public int add(int number1, int number2){
+        return number1 + number2;
+    }
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+
+    private Calculator calculator;
+
+    @Before
+        public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+        public void canAdd(){
+        assertEquals(4, calculator.add(2,2));
+    }
+
+    @Test
+        public void canSubtract
+
+}
